942c81e062a4cc432d4114a3ce31ed55a181cd4b
toadlabsweb <93641736+toadlabsweb@users.noreply.github.com>
2024-04-23 16:57:45 +0530

    Add files via upload

---
+++ lib/constants.tsx
@@ -0,0 +1,7 @@
+export const BASE_PATH = 'http://localhost:3000'
+export const SITE_TITLE = 'Toadlabs'
+export const SITE_DESC = '  We are a holographic/mixed reality solution company that enhances the connection between your business or organization and your consumers through cutting-edge 3D visualization technology in real-time. | Toadlabs'
+export const SITE_KEYWORDS = [ "Toadlabs", "Holographic", ]
+export const baseURL='/'
+export const imgblurDataURL='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mPsqQcAAZ0BDTSBuAQAAAAASUVORK5CYII='
+export const HOME_OG_IMAGE_URL ='https://toadlabs.com/public/images/logo.png'
